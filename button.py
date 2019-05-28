1aeb314aafc2d0c0ee1b3fdbddcb2213a76312b9
Tristan <admin@tristans-air.lan>
2019-05-28 15:51:12 -0700

    Fixed code

--- button.py
+++ button.py
@@ -6,26 +6,33 @@ GPIO.setmode(GPIO.BCM)
 blinkCount = 3
 count = 0
 yellow = 22
-button = 12
+button = 5
 
-# Setup the pin that the LED is connected to
+# Setup the pin the LED is connected to
 GPIO.setup(yellow, GPIO.OUT)
+# Setup the button
 GPIO.setup(button, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 
 buttonPress = True
-LEDstate = False
+ledState = False
 
-
-print("Press the button homie!")
-buttonPress = GPIO.input(button)
-if buttonPress == False and LEDstate == False:
-	GPIO.output(yellow, True)
-	print("LED ON")
-	LEDstate = True
-	sleep(2)
-elif buttonPress == True and LEDstate == True:
-	GPIO.output(yellow, False)
-	print("LED OFF")
-	LEDstate = False
-	sleep(2)
-GPIO.cleanup()                              
+try:
+    while count < blinkCount:
+        print("Come on man, press the button!")
+        buttonPress = GPIO.input(button)
+        if buttonPress == False and ledState == False:
+            GPIO.output(yellow, True)
+            print("LED ON")
+            ledState = True
+            sleep(3)
+        elif buttonPress == False and ledState == True:
+            GPIO.output(yellow, False)
+            print("LED OFF")
+            ledState = False
+            count += 1
+            sleep(0.5)
+        sleep(0.1)
+finally:
+    # Reset the GPIO Pins to a safe state
+    GPIO.output(yellow, False)
+    GPIO.cleanup()
