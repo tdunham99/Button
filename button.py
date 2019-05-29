54e74385ce9629fc04a41103c93236bd2e7492f1
Tristan <t.dunham99@gmail.com>
2019-05-29 02:24:49 +0100

    Added first file

--- button.py
+++ button.py
@@ -4,11 +4,17 @@ from time import sleep
 GPIO.setmode(GPIO.BCM)
 
 yellow = 22
-button = 12
+button = 6
+button2 = 5
+GPIO.setup(yellow, GPIO.OUT)
+GPIO.setup(button, GPIO.IN)
+GPIO.setup(button2, GPIO.IN)
 
-GPIO.setup(12, RPIO.IN, pull_up_down = GPIO.PUD_UP)
-
-if GPIO.input(button) == False:
-	print("Working")
+while True:
+	if GPIO.input(button) == False or GPIO.input(button2) == False:
+		GPIO.output(yellow, True)
+	elif GPIO.input(button) == True or GPIO.input(button2) == True:
+		GPIO.output(yellow, False)
 
 GPIO.cleanup()
+
